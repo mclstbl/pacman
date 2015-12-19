c288023fe03ed30b3e88258b9b8db7bb805fe63e
Micaela Estabillo <micaela.estabillo@gmail.com>
2015-12-19 15:03:41 -0500

    use GLUT_BITMAP to display text

--- text.cpp
+++ text.cpp
@@ -12,14 +12,22 @@
 #include <stdio.h>
 
 float positionT[3] = {0,0,7.0};
-char* characters;
+const char* characters;
 
-void Text::drawText(void)
-{
-  printf("%s\n", characters);
+
+void Text::drawText(int length, float x , float y){
+	glMatrixMode(GL_PROJECTION);
+	double *matrix = new double[16];
+	glGetDoublev(GL_PROJECTION_MATRIX, matrix);
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glRasterPos2f(x, y);
+	for(int i=0; i<length; i++){
+		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, (int)characters[i]);
+	}
+	glPopMatrix();
 }
 
-void Text::setText(char * c)
-{
+void Text::setText(char * c){
 	this -> characters = c;
 }
