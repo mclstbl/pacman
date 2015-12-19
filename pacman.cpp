9f54a8d171502b67dae19af0f99dba3e7e74dd8d
Micaela Estabillo <micaela.estabillo@gmail.com>
2015-12-19 01:16:25 -0500

    make pacman turn grey when lives < 0

--- pacman.cpp
+++ pacman.cpp
@@ -12,8 +12,46 @@
 
 float position[3] = {0.0,0.0,6.0};
 float direction[3] = {0,1,0};
-int lives = 3;
-int score = 0;
+int lives_p = 3;
+int score_p = 0;
+float angle_p = 0;
+float color_p[3] = {1.0,1.0,0.0};
+
+int Pacman::getDirection()
+{
+	for (int i = 0; i < 3; i ++)
+	{
+		if (this -> direction[i] != 0)
+			return i;
+	}
+
+	return 0;
+}
+
+int Pacman::getLives(void)
+{
+	return this -> lives_p;
+}
+
+void Pacman::deleteLife(void)
+{
+	this -> lives_p --;
+}
+
+int Pacman::getScore(void)
+{
+	return this -> score_p;
+}
+
+void Pacman::addScore(int s)
+{
+	this -> score_p += 10;
+}
+
+float Pacman::getHeight(void)
+{
+  return (13 * 0.1);
+}
 
 void Pacman::drawPacman(void)
 {
@@ -34,7 +72,14 @@ void Pacman::drawPacman(void)
      xxxxx
   */
 
-  glColor3f(1.0,1.0,0.0);    
+  if (position[2] >= 6)
+  {
+    glColor3fv(color_p);
+  }
+  else
+  {
+  	glColor3f(0.8,0.8,0.8);
+  }
 
   float vertices[13][4][3] = {
   	{{3,0,0},{7,0,0},{7,1,0},{3,1,0}},
@@ -52,10 +97,33 @@ void Pacman::drawPacman(void)
    	{{3,-12,0},{7,-12,0},{7,-11,0},{3,-11,0}}
   };
 
+  // pacman faces movement direction
+  if (direction[0] == -1)
+  {
+  	//left
+  	angle_p = 0;
+  }
+  else if (direction[0] == 1)
+  {
+  	//right
+  	angle_p = 180;
+  }
+  else if (direction[1] == 1)
+  {
+  	//up
+  	angle_p = 270;
+  }
+  else
+  {
+  	//down
+  	angle_p = 90;
+  }
+
   glPushMatrix();
 
   glTranslatef(position[0],position[1],position[2]);
-  glScalef(0.05,0.05,0.05);
+  glRotatef(angle_p,0,0,1);
+  glScalef(0.1,0.1,0.1);
 
   for (int i = 0; i < 13; i ++)
   {
--- pacman.h
+++ pacman.h
@@ -7,10 +7,18 @@ class Pacman
 public:
 	float position[3];
 	float direction[3];
-	int lives;
-	int score;
+	int lives_p;
+	int score_p;
+	float color_p[3];
 
 	void drawPacman(void);
 	void move(void);
 	void changeDirection(int d);
+	int getLives(void);
+	int getScore(void);
+	int getDirection(void);
+	float getHeight(void);
+
+	void deleteLife(void);
+	void addScore(int);
 };
