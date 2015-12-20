281ba029197b998c5942e0f153bef43d075b7821
Micaela Estabillo <micaela.estabillo@gmail.com>
2015-12-20 02:03:42 -0500

    remove accidental file push

--- stringOps.hs
+++
@@ -1,7 +0,0 @@
-stringToCharList :: String -> String
-stringToCharList [] = []
-stringToCharList (a:as) = a : "','" ++ stringToCharList as
-
-strC :: String -> String
-strC s = "{'" ++ take (length t - 2) t ++  "}"
-  where t = stringToCharList s
