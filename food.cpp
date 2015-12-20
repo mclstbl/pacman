194f7b4d3e3a3dfe09a268f5747a17f607da7f15
Micaela Estabillo <micaela.estabillo@gmail.com>
2015-12-20 17:01:47 -0500

    rescale food

--- food.cpp
+++ food.cpp
@@ -18,7 +18,7 @@
 
 float positionF[3] = {0.0,0.0,6.0}; //setting position of the food
 
-void Food::drawFood(bool n){	//drawFood function of object Food
+void Food::drawFood(bool n,int shine){	//drawFood function of object Food
 	// generate new random location of food if n==true
 	if (n == true){
 	  int x = rand() % 5 * pow(-1,rand() % 2);	//creating a random x position
@@ -34,10 +34,15 @@ void Food::drawFood(bool n){	//drawFood function of object Food
     {0,0,0},{4,0,0},{4,4,0},{0,4,0}
   };
 
+  float specs[2][4][3] = {
+    {{0,0,8},{0.5,0,8},{0.5,0.5,8},{0,0.5,8}},
+    {{0,0,8},{0.7,0,8},{0.7,0.7,8},{0,0.7,8}},
+  };
+
   glPushMatrix();	//save current matrix
    
   glTranslatef(positionF[0],positionF[1],positionF[2]);	//translating the position of the food based on x and y
-  glScalef(0.07,0.07,0.07);	//scaling the object down to 7% its original size
+  glScalef(0.05,0.05,0.05);	//scaling the object down to 7% its original size
 
   glBegin(GL_POLYGON);	//begin drawing the polygon
 	for (int j = 0; j < 4; j++)	//for loop which will go through the 4 vertices of the food object
@@ -45,5 +50,21 @@ void Food::drawFood(bool n){	//drawFood function of object Food
     glVertex3fv(vertices[j]);	//drawing the vertices using the positions stored in the vertices[] array
   }
 	glEnd();	//finish drawing polygon
+
+  // add fake shine (specular light) to food
+  glPushMatrix();
+  for (int j = 0; j < 2; j ++)
+  {
+    glTranslatef(shine,shine,0.0);
+    for (int k = 0; k < 4; k ++)
+    {
+      glColor3f(0.9,0.9,0.9);
+      glBegin(GL_POLYGON);
+      glVertex3fv(specs[j][k]);
+      glEnd();
+    }
+  }
+  glPopMatrix();
+
 	glPopMatrix();	//revert modelview matrix
 }
--- food.h
+++ food.h
@@ -7,5 +7,5 @@ class Food{	//Food Class
 public:	//public access modifiers
 	float positionF[3] = {0.0,0.0,6.0};	//setting position of the food
 
-	void drawFood(bool);	//drawFood Function declaration
+	void drawFood(bool,int);	//drawFood Function declaration
 };
