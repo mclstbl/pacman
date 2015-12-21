ac554b08dd9f2c5e4e470b8ffc36388c13783096
kevinb13 <kevinblake91@hotmail.com>
2015-12-20 23:17:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -61,6 +61,237 @@ bool cur_wait;
 
 float bx1,by1,bx2,by2;
 
+int WALLS_X = 31;
+int WALLS_Y = 28;
+
+int Walls_array[31][28] =
+  { 
+	{ 8, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7 },
+	{ 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+	{ 6, 0, 8, 1, 1, 7, 0, 8, 1, 1, 1, 7, 0, 2, 4, 0, 8, 1, 1, 1, 7, 0, 8, 1, 1, 7, 0, 6 },
+	{ 6, 0, 2, 11, 11, 4, 0, 2, 11, 11, 11, 4, 0, 2, 4, 0, 2, 11, 11, 11, 4, 0, 2, 11, 11, 4, 0, 6 },
+	{ 6, 0, 9, 3, 3, 10, 0, 9, 3, 3, 3, 10, 0, 9, 10, 0, 9, 3, 3, 3, 10, 0, 9, 3, 3, 10, 0, 6 },
+	{ 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+	{ 6, 0, 8, 1, 1, 7, 0, 8, 7, 0, 8, 1, 1, 1, 1, 1, 1, 7, 0, 8, 7, 0, 8, 1, 1, 7, 0, 6 },
+	{ 6, 0, 9, 3, 3, 10, 0, 2, 4, 0, 9, 3, 3, 11, 11, 3, 3, 10, 0, 2, 4, 0, 9, 3, 3, 10, 0, 6 },
+	{ 6, 0, 0, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 0, 0, 6 },
+	{ 9, 5, 5, 5, 5, 7, 0, 2, 11, 1, 1, 7, 0, 2, 4, 0, 8, 1, 1, 11, 4, 0, 8, 5, 5, 5, 5, 10 },
+	{ 0, 0, 0, 0, 0, 6, 0, 2, 11, 3, 3, 10, 0, 9, 10, 0, 9, 3, 3, 11, 4, 0, 6, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 6, 0, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 0, 6, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 6, 0, 2, 4, 0, 8, 5, 5, 1, 1, 5, 5, 7, 0, 2, 4, 0, 6, 0, 0, 0, 0, 0 },
+	{ 5, 5, 5, 5, 5, 10, 0, 9, 10, 0, 6, 0, 0, 0, 0, 0, 0, 6, 0, 9, 10, 0, 9, 5, 5, 5, 5, 5 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	{ 5, 5, 5, 5, 5, 7, 0, 8, 7, 0, 6, 0, 0, 0, 0, 0, 0, 6, 0, 8, 7, 0, 8, 5, 5, 5, 5, 5 },
+	{ 0, 0, 0, 0, 0, 6, 0, 2, 4, 0, 9, 5, 5, 5, 5, 5, 5, 10, 0, 2, 4, 0, 6, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 6, 0, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 0, 6, 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0, 6, 0, 2, 4, 0, 8, 1, 1, 1, 1, 1, 1, 7, 0, 2, 4, 0, 6, 0, 0, 0, 0, 0 },
+	{ 8, 5, 5, 5, 5, 10, 0, 9, 10, 0, 9, 3, 3, 11, 11, 3, 3, 10, 0, 9, 10, 0, 9, 5, 5, 5, 5, 7 },
+	{ 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+	{ 6, 0, 8, 1, 1, 7, 0, 8, 1, 1, 1, 7, 0, 2, 4, 0, 8, 1, 1, 1, 7, 0, 8, 1, 1, 7, 0, 6 },
+	{ 6, 0, 9, 3, 11, 4, 0, 9, 3, 3, 3, 10, 0, 9, 10, 0, 9, 3, 3, 3, 10, 0, 2, 11, 3, 10, 0, 6 },
+	{ 6, 0, 0, 0, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 0, 0, 0, 6 },
+	{ 2, 1, 7, 0, 2, 4, 0, 8, 7, 0, 8, 1, 1, 1, 1, 1, 1, 7, 0, 8, 7, 0, 2, 4, 0, 8, 1, 4 },
+	{ 2, 3, 10, 0, 9, 10, 0, 2, 4, 0, 9, 3, 3, 11, 11, 3, 3, 10, 0, 2, 4, 0, 9, 10, 0, 9, 3, 4 },
+	{ 6, 0, 0, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 0, 0, 6 },
+	{ 6, 0, 8, 1, 1, 1, 1, 11, 11, 1, 1, 7, 0, 2, 4, 0, 8, 1, 1, 11, 11, 1, 1, 1, 1, 7, 0, 6 },
+	{ 6, 0, 9, 3, 3, 3, 3, 3, 3, 3, 3, 10, 0, 9, 10, 0, 9, 3, 3, 3, 3, 3, 3, 3, 3, 10, 0, 6 },
+	{ 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+	{ 9, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10 }
+	};
+
+GLubyte list[5];
+
+void drawWalls (void)
+{
+	glColor3f(0, 0, 1);
+
+	for (int I = 0; I < WALLS_X; I++)
+	{
+		for (int j = 0; j < WALLS_Y / 2; j++)
+		{
+			glColor3f(0, 0, 1);
+			int call_this = 0;
+
+			glPushMatrix();
+			glTranslatef(-(float)WALLS_X / 2.0f, -(float)WALLS_Y / 2.0f, 0);
+
+			glTranslatef(j, WALLS_Y - I, 0);
+
+			glPushMatrix();
+			glTranslatef(0.5, 0.5, 0);
+
+			switch (Walls_array[I][j])
+			{
+				case 4:
+					glRotatef(90.0, 0, 0, 1);
+				case 3:
+					glRotatef(90.0, 0, 0, 1);
+				case 2:
+					glRotatef(90.0, 0, 0, 1);
+				case 1:
+					call_this = 1;
+					break;
+				case 6:
+					glRotatef(90.0, 0, 0, 1);
+				case 5:
+					call_this = 2;
+					break;
+				case 10:
+					glRotatef(90.0, 0, 0, 1);
+				case 9:
+					glRotatef(90.0, 0, 0, 1);
+				case 8:
+					glRotatef(90.0, 0, 0, 1);
+				case 7:
+					call_this = 3;
+					break;
+			}
+
+			glScalef(1, 1, 0.5);
+			glTranslatef(-0.5, -0.5, 0);
+			glCallList(list[call_this]);
+			glPopMatrix();
+
+			if (call_this != 0 || Walls_array[I][j] == 11)
+			{
+				glTranslatef(0, 0, -0.5);
+				glCallList(list[4]);
+			}
+			glPopMatrix();
+		}
+	}
+
+	int I;
+
+	for (I = 0; I< WALLS_X; I++)
+	{
+		for (int j = WALLS_Y - 1; j >= WALLS_Y / 2; j--)
+		{
+			glColor3f(0, 0, 1);
+			int call_this = 0;
+
+			glPushMatrix();
+
+			glTranslatef(-(float)WALLS_X / 2.0f, -(float)WALLS_Y / 2.0f, 0);
+			glTranslatef(j, WALLS_Y - I, 0);
+
+			glPushMatrix();
+			glTranslatef(0.5, 0.5, 0);
+			switch (Walls_array[I][j])
+			{
+			case 4:
+				glRotatef(90.0, 0, 0, 1);
+			case 3:
+				glRotatef(90.0, 0, 0, 1);
+			case 2:
+				glRotatef(90.0, 0, 0, 1);
+			case 1:
+				call_this = 1;
+				break;
+			case 6:
+				glRotatef(90.0, 0, 0, 1);
+			case 5:
+				call_this = 2;
+				break;
+			case 10:
+				glRotatef(90.0, 0, 0, 1);
+			case 9:
+				glRotatef(90.0, 0, 0, 1);
+			case 8:
+				glRotatef(90.0, 0, 0, 1);
+			case 7:
+				call_this = 3;
+				break;
+			}
+			glScalef(1, 1, 0.5);
+			glTranslatef(-0.5, -0.5, 0);
+			glCallList(list[call_this]);
+			glPopMatrix();
+
+			if (call_this != 0 || Walls_array[I][j] == 11)
+			{
+				glTranslatef(0, 0, -0.5);
+				glCallList(list[4]);
+			}
+			glPopMatrix();
+		}
+	}
+}
+
+void createList()
+{
+	list[1] = glGenLists(1);
+
+	glNewList(list[1], GL_COMPILE);
+
+	glBegin(GL_QUADS);
+	glColor3f(0, 0, 1);
+	glNormal3f(0.0, 1.0, 0.0);
+	glVertex3f(1.0, 1.0, 1.0);
+	glVertex3f(1.0, 1.0, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glVertex3f(0.0, 1.0, 1.0);
+	glEnd();
+
+	glEndList();
+
+	list[2] = glGenLists(1);
+	glNewList(list[2], GL_COMPILE);
+
+	glBegin(GL_QUADS);
+
+	glColor3f(0, 0, 1);
+	glNormal3f(0.0, 1.0, 0.0);
+	glVertex3f(1.0, 1.0, 1.0);
+	glVertex3f(1.0, 1.0, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glVertex3f(0.0, 1.0, 1.0);
+
+	glColor3f(0, 0, 1);
+	glNormal3f(0.0, -1.0, 0.0);
+	glVertex3f(1.0, 0.0, 0.0);
+	glVertex3f(1.0, 0.0, 1.0);
+	glVertex3f(0.0, 0.0, 1.0);
+	glVertex3f(0.0, 0.0, 0.0);
+	glEnd();
+	glEndList();
+
+	list[3] = glGenLists(1);
+
+	glNewList(list[3], GL_COMPILE);
+	glBegin(GL_QUADS);
+
+	glColor3f(0, 0, 1);
+	glNormal3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(1.0, 1.0, 1.0);
+	glVertex3f(1.0, 1.0, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glVertex3f(0.0, 1.0, 1.0);
+
+	glColor3f(0, 0, 1);
+	glNormal3f(1.0, 0.0, 0.0);
+	glVertex3f(1.0, 1.0, 0.0);
+	glVertex3f(1.0, 1.0, 1.0);
+	glVertex3f(1.0, 0.0, 1.0);
+	glVertex3f(1.0, 0.0, 0.0);
+	glEnd();
+	glEndList();
+
+	list[4] = glGenLists(1);
+
+	glNewList(list[4], GL_COMPILE);
+	glBegin(GL_QUADS);
+
+	glColor3f(0, 0, 1);
+	glNormal3f(1.0, 0.0, 1.0);
+	glVertex3f(1, 1, 1.0);
+	glVertex3f(0, 1, 1.0);
+	glVertex3f(0, 0, 1.0);
+	glVertex3f(1, 0, 1.0);
+	glEnd();
+	glEndList();
+}
+
 void endGame(void)
 {
 	endG = true;
@@ -280,6 +511,8 @@ void special(int key, int x, int y)
 	glutPostRedisplay();
 }
 
+
+
 void init(void)
 {    
 	glClearColor(0, 0, 0, 0);
@@ -292,8 +525,7 @@ void init(void)
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	W1.createList();
+	
 	
 	chaser.init(0);
 	ambusher.init(1);
@@ -430,9 +662,6 @@ bool plusLife = true;
     food1.drawFood(newFood,wiggleEyes % 2);
   }
  
-
-  W1.drawWalls();
-
   if(P1.getLives() < 0 && !newGame)
   {
  		gameOverText.drawText(0,0);
@@ -515,8 +744,29 @@ bool plusLife = true;
  		life.drawPacman(wiggleEyes);
  		glPopMatrix();
  	}
+	
+	glScalef(0.38,0.47,0.49);
+	glTranslatef(-3.8,0.6,0);
+	drawWalls();
+
+	glutPostRedisplay();
 
 	glutSwapBuffers();
+}
+
+void W_init()
+{
+
+	glEnable(GL_NORMALIZE);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(60,1.33,0.005,100);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+    gluLookAt(-1.5, 0, 40, -1.5, 0, 0, 0.0f,1.0f,0.0f);
 }
 
 int main(int argc, char** argv)
@@ -525,12 +775,17 @@ int main(int argc, char** argv)
 	
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	
-	glutInitWindowSize(600, 600);
+	glutInitWindowSize(1200, 1200);
 	glutInitWindowPosition(800, 0);
 
 	glutCreateWindow("Pacman");
-
+	
+	W_init();
+	
 	glutDisplayFunc(display);
+
+	createList();	
+
 	glutIdleFunc(idle);
 	
 	glutKeyboardFunc(keyboard);
@@ -538,8 +793,8 @@ int main(int argc, char** argv)
 
 	init();
 	
-	W1.createList();
 	
+
 	glutMainLoop();
 
 	return(0);
